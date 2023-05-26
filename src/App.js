80be9a669ddc51fa5e7ed0d992d07b12ec3bdc1c
vivirortega <vitoriarortega@gmail.com>
2023-05-25 21:59:16 -0300

    feat: add donuts charts to main page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import { Form } from "./style";
+import { Form, Data } from './style'
+import { Chart } from 'react-google-charts'
+
+export const data = [
+  ['Task', 'Hours per Day'],
+  ['Work', 11],
+  ['Eat', 2],
+  ['Commute', 2],
+  ['Watch TV', 2],
+  ['Sleep', 7],
+]
+
+export const options = {
+  title: 'My Daily Activities',
+  pieHole: 0.4,
+  is3D: false,
+}
 
 function App() {
   return (
-  <>
-  <Form>
-    <input type="text" placeholder="First Name"/>
-    <input type="text" placeholder="Last Name"/>
-    <input type="text" placeholder="Participation"/>
-    <button type="submit">SEND</button>
-  </Form>
-  </>
-  );
+    <>
+      <Form>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="Participation" />
+        <button type="submit">SEND</button>
+      </Form>
+      <Data>
+        <h1>DATA</h1>
+        <h2>
+          Insira seu primeiro nome, segundo nome e % de participação para ver o
+          gráfico.
+        </h2>
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+          width={'100%'}
+          height={'400px'}
+        />
+      </Data>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/style.js
+++ src/style.js
@@ -1,36 +1,55 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Form = styled.form`
-    display: flex;
-    width: 100%;
-    height: 10em;
+  display: flex;
+  width: 100%;
+  height: 10em;
+  background-color: #00b8e2;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  gap: 1em;
+  align-items: center;
+  justify-content: center;
+
+  input {
+    width: 20em;
+    height: 3.5em;
+    border: none;
+    border-radius: 2px;
+  }
+
+  button {
+    width: 10em;
+    height: 3.1em;
+    border: 2px solid #ffffff;
+    border-radius: 2px;
     background-color: #00b8e2;
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    gap: 1em;
-    align-items: center;
-    justify-content: center;
-
-
-    input {
-        width: 20em;
-        height: 3.50em;
-        border: none;
-        border-radius: 2px;
-    }
-
-    button {
-        width: 10em;
-        height: 3.10em;
-        border: 2px solid #ffffff;
-        border-radius: 2px;
-        background-color: #00b8e2;
-        color: #ffffff;
-        font-weight: bold;
-        font-family: 'Poppins', Arial, Helvetica, sans-serif;
-        font-size: 1em;
-    }
-
+    color: #ffffff;
+    font-weight: bold;
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-size: 1em;
+  }
 `
-export {Form};
+
+const Data = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  h1 {
+    color: #333333;
+    margin-top: 5em;
+    font-size: 3em;
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-weight: bolder;
+  }
+
+  h2 {
+    font-size: 1.3em;
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-weight: 400;
+  }
+`
+export { Form, Data }
