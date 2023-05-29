6a7750318602ab4f941de716b5c2882a1e632dd3
vivirortega <vitoriarortega@gmail.com>
2023-05-29 19:07:40 -0300

    feat: connection to backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
 import { Form, Data } from './style'
 import { Chart } from 'react-google-charts'
+import axios from 'axios'
+import { useState } from 'react'
 
 export const data = [
   ['Task', 'Hours per Day'],
@@ -17,12 +19,33 @@ export const options = {
 }
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [participation, setParticipation] = useState(0);
+
+  function postUser(event){
+    event.preventDefault();
+
+    const data = {firstName, lastName, participation};
+    const promise = axios.post('http://localhost:5000/', data);
+    promise.then((response) => {
+      console.log(response)
+      setFirstName("");
+      setLastName("");
+      setParticipation("");
+    });
+    promise.catch((error) => {
+      console.log(error)
+    })
+
+  }
+
   return (
     <>
-      <Form>
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Participation" />
+      <Form onSubmit={postUser}>
+        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" required />
+        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" required />
+        <input type="text" value={participation} onChange={(e) => setParticipation(e.target.value)} placeholder="Participation" required />
         <button type="submit">SEND</button>
       </Form>
       <Data>
